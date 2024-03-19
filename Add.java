c8ff1d4eebc5e4b486e6cc81205c677090236462
prasadlomte05 <163580868+prasadlomte05@users.noreply.github.com>
2024-03-19 23:43:23 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Add
+
+{
+
+	public static void main(String[]args) 
+
+	{
+
+		Scanner sc = new Scanner(System.in);
+	
+		System.out.println("Enter1");
+	
+		 int a = sc.nextInt();
+		
+		System.out.println("Enter2");
+	
+		 int b = sc.nextInt();
+		 int c= a+b;
+		 System.out.println("sum-"+c);
+
+		 
+	}
+
+}
+
