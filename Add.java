1981f9b2ba49dcb38e4fb46c85310d90f3b205c6
prasadlomte05 <163580868+prasadlomte05@users.noreply.github.com>
2024-03-19 23:47:20 +0530

    Update Add.java

--- Add.java
+++ Add.java
@@ -1,27 +1,15 @@
 import java.util.Scanner;
-
 class Add
-
 {
-
 	public static void main(String[]args) 
-
 	{
-
-		Scanner sc = new Scanner(System.in);
-	
+		Scanner sc = new Scanner(System.in);	
 		System.out.println("Enter1");
-	
-		 int a = sc.nextInt();
-		
+		int a = sc.nextInt();
 		System.out.println("Enter2");
-	
-		 int b = sc.nextInt();
-		 int c= a+b;
-		 System.out.println("sum-"+c);
-
-		 
+		int b = sc.nextInt();
+		int c= a+b;
+		System.out.println("sum-"+c);
 	}
-
 }
 
