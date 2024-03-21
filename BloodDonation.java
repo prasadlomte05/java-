fbd824aba7ea57257560ac33ca97ad4501f39bd4
prasadlomte05 <163580868+prasadlomte05@users.noreply.github.com>
2024-03-22 00:43:26 +0530

    Update BloodDonation.java

--- BloodDonation.java
+++ BloodDonation.java
@@ -6,14 +6,14 @@ public static void main(String[] args) {
 	System.out.println("Enter Your Age-");
 	int Age=sc.nextInt();
 	if(Age<20){
-			System.out.println("Not Adalt");
-	}else{
+		System.out.println("Not Adalt");
+	} else{
 		System.out.println("Have you consume cigar or alcohol (yes/no)");
 		String nasha=sc.next().toLowerCase();		
 		if(nasha.equals("no")){
 			System.out.println("Enter your Weight");
 			double Weight=sc.nextDouble();
-			if(Weight<50){
+				if(Weight<50){
 				System.out.println("over Weight");
 			}else{
 				System.out.println("Donet Your Blood");
